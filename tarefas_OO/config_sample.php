cd2f9d1fe7246bfaf748f80345fcd6881a9421ca
Raul <rauldionisiosh@gmail.com>
2019-07-16 09:17:40 -0300

    Reinserindo arquivo config

---
+++ tarefas_OO/config_sample.php
@@ -0,0 +1,15 @@
+<?php
+ // Conexão com o banco de dados MySql
+
+ define("BD_SERVIDOR","");//insesir servidor entre as aspas
+ define("BD_USUARIO","");//inserir usuário entre as aspas 
+ define("BD_SENHA","");//inserir senha entre as aspas
+ define("BD_BANCO","");//inserir nome do banco entre as aspas
+
+ //EMAIL para a notificação
+ define("EMAIL_NOTIFICACAO", "");
+
+ //Senha para envio de mail
+ define("PASSWORD", "");
+
+?>
